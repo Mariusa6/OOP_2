9906b360b9bf4ad938c485ae2935f19a03d62969
Mariusa6 <marius.augustinas@mif.stud.vu.lt>
2026-07-31 11:46:45 +0300

    v1.1 3 - getter'iai

--- OOP_2/studentas.cpp
+++ OOP_2/studentas.cpp
@@ -19,3 +19,24 @@ std::istream& studentas::readStudentas(std::istream& is) {
 	is.clear();
 	return is;
 }
+
+double studentas::getGalutinisVid() const {
+	if (nd_.empty()) return 0.0;
+	double ndVid = std::accumulate(nd_.begin(), nd_.end(), 0.0) / nd_.size();
+	return ndVid * namuDarbaiSvoris + egzaminas_ * egzaminasSvoris;
+}
+
+double studentas::getGalutinisMed() const {
+	if (nd_.empty()) return 0.0;
+	std::vector<int> ndCopy = nd_;
+	std::sort(ndCopy.begin(), ndCopy.end());
+	double ndMed;
+	size_t size = ndCopy.size();
+	if (size % 2 == 0) {
+		ndMed = (ndCopy[size / 2 - 1] + ndCopy[size / 2]) / 2.0;
+	}
+	else {
+		ndMed = ndCopy[size / 2];
+	}
+	return ndMed * namuDarbaiSvoris + egzaminas_ * egzaminasSvoris;
+}
--- OOP_2/studentas.h
+++ OOP_2/studentas.h
@@ -4,6 +4,8 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <numeric>
+#include <algorithm>
 
 // -------------------------------------------------------
 // class studentas
@@ -21,6 +23,12 @@ private:
 	double galutinisMed_;
 
 public:
+	// Konstantos
+	static constexpr int minPazymys = 1;
+	static constexpr int maxPazymys = 10;
+	static constexpr double namuDarbaiSvoris = 0.4;
+	static constexpr double egzaminasSvoris = 0.6;
+
 	// Konstruktoriai
 	studentas() :   vardas_(""),			// default
 					pavarde_(""),
@@ -32,6 +40,14 @@ public:
 
 	// Destruktorius
 	~studentas() {}
+
+	// Getter'iai
+	inline std::string getVardas() const { return vardas_; }
+	inline std::string getPavarde() const { return pavarde_; }
+	inline std::vector<int> getNamuDarbai() const { return nd_; }
+	inline int getEgzaminas() const { return egzaminas_; }
+	inline double getGalutinisVid() const;
+	inline double getGalutinisMed() const;
 
 	// Setter'iai
 	std::istream& readStudentas(std::istream& is);	// skaito studento duomenis
