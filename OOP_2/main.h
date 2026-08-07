e48589769a4b15747d66c9c06b572bf69eb29ef0
Mariusa6 <marius.augustinas@mif.stud.vu.lt>
2026-08-07 12:02:32 +0300

    v1.1 6 - pasalintas calculate.cpp pakoreguotas main pagal klase studentas

--- OOP_2/main.h
+++ OOP_2/main.h
@@ -11,9 +11,13 @@
 #include <list>         // list
 #include <deque>        // deque
 #include <exception>    // exception
+#include "studentas.h"  // class studentas
 
 #define NUMBER_OF_PAZYMYS 15
 
+// -------------------------------------------------------
+// splitResult — 1 strategijos rezultatas (du nauji konteineriai)
+// -------------------------------------------------------
 template<typename Container>
 struct splitResult
 {
@@ -21,13 +25,15 @@ struct splitResult
     Container vargsiukai;
 };
 
-const int minPazymys{ 1 };
-const int maxPazymys{ 10 };
-
 // Function declarations
 
 // Menu functions
 char askMenuChoice();
+int enterNumberOfStudents();
+char askSortBy();
+bool askIfMoreStudents();
+char askOutputChoice();
+char askContainerChoice();
 
 // Generate functions
 std::vector<int> generatePazymiai(int m);
@@ -37,8 +43,6 @@ void printWelcome();
 void printNameAsk();
 
 // User input functions
-int enterNumberOfStudents();
-bool askIfMoreStudents();
 studentas enterStudentas(int n);
 std::string enterName(int n);
 std::string enterSurname(int n);
@@ -46,13 +50,6 @@ int enterNumberOfPazymys(int n);
 int enterPazymys(int n);
 std::vector<int> enterPazymiai(int n, int m);
 int enterEgzaminas(int n);
-char askAverageOrMedian();
-char askSortBy();
-char askOutputChoice();
-char askContainerChoice();
-
-// Calculation functions
-double mediana(const std::vector<int>& namuDarbai);
 
 // File handling functions
 std::string enterFileName();
