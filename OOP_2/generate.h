32bea1a28493029fdf1b512207eb8362695653e7
Mariusa6 <marius.augustinas@mif.stud.vu.lt>
2026-08-07 12:09:03 +0300

    v1.1 9 - pakoreguotas generate.h

--- OOP_2/generate.h
+++ OOP_2/generate.h
@@ -8,7 +8,8 @@
 
 // -------------------------------------------------------
 // Template: generateStudentai
-// Generuoja studentus su atsitiktiniais vardais, pavardėmis ir pažymiais.
+// Generuoja studentus su šabloniniais vardais (VardasNR1,
+// PavardeNR1, ...) ir atsitiktiniais pažymiais.
 // -------------------------------------------------------
 template<typename Container>
 Container generateStudentai(int n)
@@ -16,34 +17,30 @@ Container generateStudentai(int n)
     Container studentai;
     for (int i = 0; i < n; i++)
     {
-        studentas s;
-        s.vardas = "VardasNR" + std::to_string(i + 1);
-        s.pavarde = "PavardeNR" + std::to_string(i + 1);
-        int m = NUMBER_OF_PAZYMYS;
-        s.namuDarbai = generatePazymiai(m);
-        s.egzaminas = generatePazymiai(1)[0];
-        studentai.push_back(s);
+        studentai.push_back(studentas(
+            "VardasNR" + std::to_string(i + 1),
+            "PavardeNR" + std::to_string(i + 1),
+            generatePazymiai(NUMBER_OF_PAZYMYS),
+            generatePazymiai(1)[0]));
     }
     return studentai;
 }
 
 // -------------------------------------------------------
 // Template: generateOnlyPazymiai
-// Generuoja tik atsitiktinius pažymius studentams.
+// Vardai ir pavardės įvedami rankiniu būdu, pažymiai generuojami.
 // -------------------------------------------------------
 template<typename Container>
 Container generateOnlyPazymiai(int n)
 {
     Container studentai;
-    studentas s;
     for (int i = 0; i < n; i++)
     {
-        s.vardas = enterName(i + 1);
-        s.pavarde = enterSurname(i + 1);
-        int m = NUMBER_OF_PAZYMYS;
-        s.namuDarbai = generatePazymiai(m);
-        s.egzaminas = generatePazymiai(1)[0];
-        studentai.push_back(s);
+        studentai.push_back(studentas(
+            enterName(i + 1),
+            enterSurname(i + 1),
+            generatePazymiai(NUMBER_OF_PAZYMYS),
+            generatePazymiai(1)[0]));
     }
     return studentai;
 }
